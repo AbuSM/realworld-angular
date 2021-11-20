f6710c3efa1ac52ed36131b7e8ebede1a2ded4a9
Abdufatoh <android17cpu@gmail.com>
2021-11-21 02:34:09 +0500

    Refactor

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SharedModule } from '../shared/shared.module';
+import { SharedModule } from '../shared/';
 import { HeaderComponent, MainComponent } from './';
 import { RouterModule } from '@angular/router';
 
