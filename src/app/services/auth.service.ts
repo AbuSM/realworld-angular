18812b743cef0d503a54886988e67d7016a00032
Abdufatoh <android17cpu@gmail.com>
2021-11-21 02:12:41 +0500

    changes in services
    fixed apiService: removed HttpParams from get method, renamed params to config
    created articles service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@angular/core';
-import { UserCredentialsModel } from '../models';
+import { UserCredentialsModel, UserModel } from '../models';
 import { ApiService } from './api.service';
 import { map } from 'rxjs';
+import { clear, getItem, setItem } from '../utils';
+import { Store } from '@ngrx/store';
+import { authorize } from '../auth/+store/auth.actions';
 
 @Injectable()
 export class AuthService {
-    constructor(private apiService: ApiService) {}
+    constructor(
+        private apiService: ApiService,
+        private store: Store<{ auth: object }>
+    ) {}
 
     authUser(authType: string, credentials: UserCredentialsModel) {
         return this.apiService
@@ -14,13 +20,30 @@ export class AuthService {
             })
             .pipe(
                 map((data) => {
-                    this.setAuth(data);
+                    this.setAuth(data.user);
                     return data;
                 })
             );
     }
 
-    setAuth(user) {
-        console.log(user);
+    setAuth(user: UserModel) {
+        setItem(user.token);
+    }
+
+    logout() {
+        clear();
+    }
+
+    checkUser() {
+        const token = getItem();
+        if (token) {
+            this.apiService
+                .get('user', { headers: { Authorization: `Bearer ${token}` } })
+                .subscribe({
+                    next: () => this.store.dispatch(authorize()),
+                });
+        } else {
+            this.logout();
+        }
     }
 }
