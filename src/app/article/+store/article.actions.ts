6a28393511fd9873f0ff2da7e9bd712b9c4a7497
Abdufatoh <android17cpu@gmail.com>
2021-11-29 17:52:38 +0500

    add footer component, remove unused variables

--- src/app/article/+store/article.actions.ts
+++ src/app/article/+store/article.actions.ts
@@ -1,5 +1,5 @@
 import { createAction, props } from '@ngrx/store';
-import { ArticleModel } from '../../models';
+import { ArticlesWrapModel } from '../../models';
 
 export const ARTICLE_FETCH_ALL = '[Article Component] Fetch';
 export const ARTICLE_FETCH_ALL_SUCCESS = '[Article Component] Fetch Success';
@@ -9,14 +9,11 @@ export const ARTICLE_FETCH_FEED = '[Article Component] Fetch Feed';
 export const ARTICLE_FAVORITE_ACTION = '[Article Component] Favorite';
 export const ARTICLE_FAVORITE_SUCCESS = '[Article Component] Favorite Success';
 export const ARTICLE_FAVORITE_FAILURE = '[Article Component] Favorite Failure';
-export const ARTICLE_FOLLOW_ACTION = '[Article Component] Follow';
-export const ARTICLE_FOLLOW_SUCCESS = '[Article Component] Follow Success';
-export const ARTICLE_FOLLOW_FAILURE = '[Article Component] Follow Failure';
 
 export const fetchAllArticles = createAction(ARTICLE_FETCH_ALL);
 export const fetchAllArticlesSuccess = createAction(
     ARTICLE_FETCH_ALL_SUCCESS,
-    props<{ articles: ArticleModel[] }>()
+    props<ArticlesWrapModel>()
 );
 export const fetchAllArticlesFailure = createAction(
     ARTICLE_FETCH_ALL_FAILURE,
@@ -36,15 +33,5 @@ export const onToggleFavoriteSuccess = createAction(
 
 export const onToggleFavoriteFailure = createAction(
     ARTICLE_FAVORITE_FAILURE,
-    (payload) => payload
-);
-
-export const onToggleFollow = createAction(ARTICLE_FOLLOW_ACTION);
-export const onToggleFollowSuccess = createAction(
-    ARTICLE_FOLLOW_SUCCESS,
-    (payload) => payload
-);
-export const onToggleFollowFailure = createAction(
-    ARTICLE_FOLLOW_FAILURE,
     (payload) => payload
 );
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/';
 import { HeaderComponent } from './';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-    declarations: [HeaderComponent],
+    declarations: [HeaderComponent, FooterComponent],
     imports: [CommonModule, SharedModule, RouterModule],
-    exports: [HeaderComponent],
+    exports: [HeaderComponent, FooterComponent],
 })
 export class LayoutModule {}
